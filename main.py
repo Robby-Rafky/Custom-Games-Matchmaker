80b79751d17155e950f9f78f0d9a512fc4e276ed
Im-a-degen <robbyrafky@gmail.com>
2023-02-27 16:55:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import customtkinter
+
+root = customtkinter.CTk()
+
+root.mainloop()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    pass
+
